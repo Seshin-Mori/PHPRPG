e21d8e73414e581eedf430a9e10ca5f4b25bc73d
seshin mori <rap.97268@gmail.com>
2021-11-21 22:56:01 +0900

    継承を追加

--- classes/Human.php
+++ classes/Human.php
@@ -5,20 +5,21 @@ class Human
 //プロパティ
     const MAX_HITPOINT = 100; //最大HP
     public $name; //名前
-    public $hitPoint; //HP
-    public $attackPoint; //攻撃力
+    public $hitPoint = 100; //HP
+    public $attackPoint = 20; //攻撃力
+
 
 //メソッド
     public function doAttack($enemy)
     {
         echo "『" . $this->name . "』の攻撃！\n";
-        echo "【" . $this->name . "】に" . $this->attackPoint . "のダメージ \n";
+        echo "【" . $enemy->name . "】に" . $this->attackPoint . "のダメージ \n";
         $enemy->tookDamage($this->attackPoint);
     }
 
-    public function tookDamege($damage)
+    public function tookDamage($damage)
     {
-        $this->hitpoint -= $damage;
+        $this->hitPoint -= $damage;
         if ($this->hitPoint < 0) {
             $this->hitPoint = 0;
         }
--- main.php
+++ main.php
@@ -2,11 +2,35 @@
 
 //echo "処理開始\n\n";
 //ファイルのロード
-require_once('./classes/Human.php');
-require_once('./classes/Enemy.php');
+require_once './classes/Human.php';
+require_once './classes/Enemy.php';
+require_once './classes/Brave.php';
 //インスタンス化
-$cecil = new Human();
+//$cecil = new Human();
+$cecil = new Brave();
 $goblin = new Enemy();
 
 $cecil->name = "セシル";
 $goblin->name = "ゴブリン";
+
+$turn = 1;
+
+while ($cecil->hitPoint > 0 && $goblin->hitPoint > 0) {
+    echo "*** $turn ターン目 ***\n\n";
+//現在のHP
+    //::オブジェクト定数
+    echo $cecil->name . " : " . $cecil->hitPoint . "/" . $cecil::MAX_HITPOINT . "\n";
+    echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+    echo "\n";
+
+    $cecil->doAttack($goblin);
+    echo "\n";
+    $goblin->doAttack($cecil);
+    echo "\n";
+
+    $turn++;
+}
+//終了後
+echo "戦闘終了\n";
+echo $cecil->name . " : " . $cecil->hitPoint . "/" . $cecil::MAX_HITPOINT . "\n";
+echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
